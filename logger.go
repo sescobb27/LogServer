9ef439a15f5da818f1faa55e6eb9e454703a8bd3
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2014-05-18 01:23:24 -0500

    AsyncWrite with buffered channels on every file in log_files array

--- logger.go
+++ logger.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+        "bufio"
+        "errors"
         "os"
 )
 
+var (
+        NoSuchFile error = errors.New("Error: No such file in Logger")
+)
+
+type File struct {
+        path    string
+        msg     chan []byte
+}
+
 type Logger struct {
-        log_files []string
+        log_files []*File
 }
 
 func assertNoError(err error) {
@@ -31,32 +42,27 @@ func verifyPath(path string) error {
 }
 
 func newLogger() *Logger {
-        logger := &Logger{log_files: make([]string, 0)}
+        logger := &Logger{log_files: make([]*File, 0, 20)}
         return logger
 }
 
-func (l Logger) exist(fn func(string) bool) bool {
+func (l Logger) exist(path string) (*File, bool) {
         for _, log_file := range l.log_files {
-                if fn(log_file) {
-                        return true
+                if path == log_file.path {
+                        return log_file, true
                 }
         }
-        return false
+        return nil, false
 }
 
 func (l *Logger) AddLogFile(path string, empty *struct{}) error {
         if err := verifyPath(path); err != nil {
                 return err
         }
-
-        fn := func(log_file string) bool {
-                if path == log_file {
-                        return true
-                }
-                return false
-        }
-        if !(*l).exist(fn) {
-                l.log_files = append(l.log_files, path)
+        if _, ok := (*l).exist(path); !ok {
+                log_file := &File{path: path, msg: make(chan []byte, 10)}
+                l.log_files = append(l.log_files, log_file)
+                go log_file.listen()
         }
         return nil
 }
@@ -69,4 +75,30 @@ func (l *Logger) AddLogsFile(paths []string, empty *struct{}) error {
                 }
         }
         return nil
+}
+
+func (l *Logger) AsyncWrite(path string, msg []byte) error {
+        if log_file, ok := (*l).exist(path); ok {
+                log_file.msg <- msg
+                return nil
+        }
+        return NoSuchFile
+}
+
+func (f *File) listen() {
+        for bytes := range f.msg {
+                file, err := os.OpenFile(f.path, os.O_RDWR|os.O_APPEND, 0660)
+                assertNoError(err)
+
+                writer := bufio.NewWriter(file)
+
+                _, err = writer.Write(bytes)
+                if err != nil {
+                        file.Close()
+                        assertNoError(err)
+                        continue
+                }
+                writer.Flush()
+                file.Close()
+        }
 }
