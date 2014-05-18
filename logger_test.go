92771eb5c3dc48d5f8bff501284d86bb8bafbc59
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2014-05-17 23:25:48 -0500

    Benchmarking Write Operations for best approach on Log Server

--- logger_test.go
+++ logger_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+        "bufio"
         "os"
         "testing"
 )
@@ -74,5 +75,67 @@ func TestAddMultipleNoPermissionLogFiles(t *testing.T) {
         }
         if len(log.log_files) != 0 {
                 failTest(t, 0, len(log.log_files))
+        }
+}
+
+func BenchmarkWriteByte(b *testing.B) {
+        msg := []byte("Hello World!!!\n")
+        for i := 0; i < b.N; i++ {
+                b.StopTimer()
+                file, err := os.OpenFile("./logfile0.log", os.O_RDWR|os.O_APPEND, 0777)
+                assertNoError(err)
+
+                writer := bufio.NewWriter(file)
+                b.StartTimer()
+                for _, v := range msg {
+                        err := writer.WriteByte(v)
+                        if err != nil {
+                                file.Close()
+                                assertNoError(err)
+                                return
+                        }
+                }
+                writer.Flush()
+                file.Close()
+        }
+}
+
+func BenchmarkWriteString(b *testing.B) {
+        msg := "Hello World!!!\n"
+        for i := 0; i < b.N; i++ {
+                b.StopTimer()
+                file, err := os.OpenFile("./logfile1.log", os.O_RDWR|os.O_APPEND, 0777)
+                assertNoError(err)
+
+                writer := bufio.NewWriter(file)
+                b.StartTimer()
+                _, err = writer.WriteString(msg)
+                if err != nil {
+                        file.Close()
+                        assertNoError(err)
+                        return
+                }
+                writer.Flush()
+                file.Close()
+        }
+}
+
+func BenchmarkWrite(b *testing.B) {
+        msg := []byte("Hello World!!!\n")
+        for i := 0; i < b.N; i++ {
+                b.StopTimer()
+                file, err := os.OpenFile("./logfile2.log", os.O_RDWR|os.O_APPEND, 0777)
+                assertNoError(err)
+
+                writer := bufio.NewWriter(file)
+                b.StartTimer()
+                _, err = writer.Write(msg)
+                if err != nil {
+                        file.Close()
+                        assertNoError(err)
+                        return
+                }
+                writer.Flush()
+                file.Close()
         }
 }
