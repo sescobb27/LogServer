bd731ec295023235a39a28a6ba3b34a96c12d12c
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2014-05-30 23:25:03 -0500

    Tcp Server For Logs

--- log_server.go
+++ log_server.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+        "encoding/json"
+        "fmt"
         "net"
-        "net/http"
-        "net/rpc"
 )
 
 var (
@@ -14,14 +14,34 @@ func init() {
         logger = newLogger()
 }
 
-func startServer() {
-        listener, err := net.Listen("tcp", ":9000")
-        assertNoError(err)
-        go http.Serve(listener, nil)
+func request_handler(conn net.Conn) {
+        buffer := make([]byte, 0, 4096)
+        var request map[string]string
+        for {
+                _, err := conn.Read(buffer)
+                if err != nil {
+                        conn.Close()
+                        fmt.Println(err)
+                }
+                conn.Close()
+                json.Unmarshal(buffer, &request)
+                logger.AddLogFile(request["logfile"])
+                logger.AsyncWrite(request["logfile"], []byte(request["msg"]))
+
+        }
 }
 
 func main() {
-        rpc.Register(logger)
-        rpc.HandleHTTP()
-        startServer()
+        listener, err := net.Listen("tcp", ":9000")
+        if err != nil {
+                fmt.Println(err)
+        }
+        defer listener.Close()
+        for {
+                conn, err := listener.Accept()
+                if err != nil {
+                        fmt.Println(err)
+                }
+                go request_handler(conn)
+        }
 }
